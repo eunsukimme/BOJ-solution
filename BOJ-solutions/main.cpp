b914e5fc052cf008465ecbe3b2f9508442c114b7
eunsukimme <eunsu.dev@gmail.com>
2020-01-27 23:21:08 +0900

    Initial Commit

---
+++ BOJ-solutions/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BOJ-solutions
+//
+//  Created by 김은수 on 27/01/2020.
+//  Copyright © 2020 김은수. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
