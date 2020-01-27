7aac3e56436f93a71d98dd7468f115068dd84ee0
eunsukimme <eunsu.dev@gmail.com>
2020-01-27 23:52:51 +0900

    Commit for XCode migration

---
+++ BOJ-solutions/BOJ_10869_사칙연산.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int boj_10869()
+{
+  int A, B;
+  cin >> A >> B;
+  cout << A + B << endl;
+  cout << A - B << endl;
+  cout << A * B << endl;
+  cout << A / B << endl;
+  cout << A % B << endl;
+  return 0;
+}
