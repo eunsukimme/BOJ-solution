333ad5927524e3fb39f35e103db4979e5435d359
eunsukimme <eunsu.dev@gmail.com>
2020-01-29 18:26:24 +0900

    Add BOJ 1904 solution(cpp)

---
+++ BOJ-solutions/BOJ_1904_타일.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#define MAX 1000001
+using namespace std;
+int f[MAX] = {0};
+int main(){
+    f[1] = 1; f[2] = 2;
+    int N;
+    cin >> N;
+    if(N <= 2){
+        cout << f[N] << endl;
+        return 0;
+    }
+    for(int i = 3 ; i <= N ; i++){
+        f[i] = ( f[i-1] + f[i-2] ) % 15746;
+    }
+    cout << f[N] % 15746 << endl;
+    return 0;
+}
--- BOJ-solutions/BOJ_2753_윤년.cpp
+++ BOJ-solutions/BOJ_2753_윤년.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int main(){
+int boj_2753(){
     int N;
     cin >> N;
     cout << ( ( (N % 4 == 0 && N % 100 != 0) || N % 400 == 0 ) ? 1 : 0 ) << endl;
