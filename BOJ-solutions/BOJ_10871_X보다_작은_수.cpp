2505911f66c21b8c95a8cb57120dd1e0c7ee7706
eunsukimme <eunsu.dev@gmail.com>
2020-02-06 17:58:08 +0900

    Add BOJ 10871 solution(cpp)

---
+++ BOJ-solutions/BOJ_10871_X보다_작은_수.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int N, X;
+    cin >> N >> X;
+    vector<int> v;
+    for(int i = 0 ;i < N ; i++){
+        int n;
+        cin >> n;
+        v.push_back(n);
+    }
+    for(int i = 0 ;i < N ;i++){
+        if(v[i] < X){
+            cout << v[i] << " ";
+        }
+    }
+    return 0;
+}
--- BOJ-solutions/BOJ_2439_별찍기_2.cpp
+++ BOJ-solutions/BOJ_2439_별찍기_2.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int main(){
+int boj_2439(){
     int N;
     cin >> N;
     for(int i = N-1 ; i >= 0 ; i--){
